e229a72b283e1d057c376347ab8f4b8d846c062a
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-13 20:16:44 -0500

    added boiler inline style to contact

--- src/contact.js
+++ src/contact.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 const ContactInfo = () => {
   return (
-    <section>
-      <h2>Contact Information</h2>
-      <p>Email: Jesus48046@gmail.com</p>
-      <p>Phone: (956) 477-7232</p>
-      <p>Address: 123 Main Street, City, Country</p>
+    <section style={styles.container}>
+      <h2 style={styles.heading}>Contact Information</h2>
+      <p style={styles.text}>Email: example@example.com</p>
+      <p style={styles.text}>Phone: (123) 456-7890</p>
+      <p style={styles.text}>Address: 123 Main Street, City, Country</p>
     </section>
   );
+};
+
+const styles = {
+  container: {
+    margin: '20px',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  },
+  heading: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '10px',
+  },
+  text: {
+    fontSize: '16px',
+    marginBottom: '5px',
+  },
 };
 
 export default ContactInfo;

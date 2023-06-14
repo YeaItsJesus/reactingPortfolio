8c86ee726c9e54bc94e5caab908ab28f62b083c4
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-13 20:13:57 -0500

    added contactinfo component

---
+++ src/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactInfo = () => {
+  return (
+    <section>
+      <h2>Contact Information</h2>
+      <p>Email: Jesus48046@gmail.com</p>
+      <p>Phone: (956) 477-7232</p>
+      <p>Address: 123 Main Street, City, Country</p>
+    </section>
+  );
+};
+
+export default ContactInfo;

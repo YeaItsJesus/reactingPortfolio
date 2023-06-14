5cb4efb958e53f84360ecf6318cca1efc6a01941
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-13 20:00:44 -0500

    adding components

---
+++ src/aboutme.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      {/* Add content  */}
+    </section>
+  );
+};
+
+export default AboutMe;
